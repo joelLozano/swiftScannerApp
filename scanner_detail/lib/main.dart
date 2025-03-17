8179c16ea6100255a4c5be3c486b2bcc3394df19
joelLozano <lozanojoel35@gmail.com>
2025-03-17 18:37:07 +0000

    initial commit

---
+++ scanner_detail/lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or press Run > Flutter Hot Reload in a Flutter IDE). Notice that the
+        // counter didn't reset back to zero; the application is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Detalle QR'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+ 
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+      title: Text(widget.title),
+      leading: IconButton(
+        icon: Icon(Icons.arrow_back),
+        onPressed: () {
+        Navigator.of(context).pop();
+        },
+      ),
+      ),
+      body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+        Icon(
+          Icons.qr_code,
+          size: 100,
+        ),
+        SizedBox(height: 20),
+        Text(
+          'Texto del QR',
+          style: TextStyle(fontSize: 24),
+        ),
+        ],
+      ),
+      ),
+    );
+  }
+}
